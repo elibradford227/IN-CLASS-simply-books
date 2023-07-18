288d61535426045c7aacf395bb40e5a25f95edbb
Eli Bradford <elibradford227@gmail.com>
2023-07-17 21:01:27 -0500

    Added delete functionality

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -1,13 +1,4 @@
-// import React from 'react';
-// import { useRouter } from 'next/router';
-
-// export default function New() {
-//   const router = useRouter();
-//   // eslint-disable-next-line no-unused-vars
-//   const { firebaseKey } = router.query;
-//   return <div>New here</div>;
-// }
-
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
@@ -27,7 +18,6 @@ export default function ViewAuthor() {
   // TODO: make call to API layer to get the data
   useEffect(() => {
     getAllTheAuthors();
-    // viewAuthorDetails(firebaseKey).then(setAuthorDetails);
   }, [firebaseKey]);
 
   return (
@@ -44,7 +34,6 @@ export default function ViewAuthor() {
           Author Email: <a href={`mailto:${authorDetails.email}`}>{authorDetails.email}</a>
           <p>{authorDetails.description || ''}</p>
           <hr />
-          {/* <BookCard bookObj={authorDetails.books} /> */}
           {authorDetails.books?.map((book) => (
             <BookCard key={book.firebaseKey} bookObj={book} onUpdate={getAllTheAuthors} />
           ))}
@@ -52,22 +41,4 @@ export default function ViewAuthor() {
       </div>
     </div>
   );
-
-  // return (
-  //   <div className="mt-5 d-flex flex-wrap">
-  //     <div className="d-flex flex-column">
-  //       <img src={authorDetails.image} alt={authorDetails.title} style={{ width: '300px' }} />
-  //     </div>
-  //     <div className="text-white ms-5 details">
-  //       <h5>
-  //         {authorDetails.title} by {authorDetails.first_name} {authorDetails.last_name}
-  //         {authorDetails.authorObject?.favorite ? ' 🤍' : ''}
-  //       </h5>
-  //       Author Email: <a href={`mailto:${authorDetails.email}`}>{authorDetails.email}</a>
-  //       <p>{authorDetails.description || ''}</p>
-  //       <hr />
-  //       <BookCard bookObj={authorDetails.books} />
-  //     </div>
-  //   </div>
-  // );
 }
